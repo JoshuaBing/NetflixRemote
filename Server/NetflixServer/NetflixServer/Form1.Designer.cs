28abd42f1a3b7984008ad423c376572c54c72cae
JoshuaBing <joshua.bing.2010@gmail.com>
2013-09-16 17:57:40 -0400

    Added code to enable/disable screensaver while
    server is running.

---
+++ Server/NetflixServer/NetflixServer/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NetflixServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.disable = new System.Windows.Forms.Button();
+            this.Enable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // disable
+            // 
+            this.disable.Location = new System.Drawing.Point(64, 12);
+            this.disable.Name = "disable";
+            this.disable.Size = new System.Drawing.Size(114, 23);
+            this.disable.TabIndex = 0;
+            this.disable.Text = "Disable Screensaver";
+            this.disable.UseVisualStyleBackColor = true;
+            this.disable.Click += new System.EventHandler(this.disable_Click);
+            // 
+            // Enable
+            // 
+            this.Enable.Location = new System.Drawing.Point(64, 101);
+            this.Enable.Name = "Enable";
+            this.Enable.Size = new System.Drawing.Size(114, 23);
+            this.Enable.TabIndex = 1;
+            this.Enable.Text = "Enable Screensaver";
+            this.Enable.UseVisualStyleBackColor = true;
+            this.Enable.Click += new System.EventHandler(this.Enable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Enable);
+            this.Controls.Add(this.disable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button disable;
+        private System.Windows.Forms.Button Enable;
+    }
+}
+
---
+++ Server/NetflixServer/NetflixServer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace NetflixServer
+{
+    public partial class Form1 : Form
+    {
+        const int screenSaverActiveFlag = 17;
+        const int screenSaverUpdateProfile = 2;
+        int screenSaverRetVal = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool SystemParametersInfo(int uAction, int uParam, ref int lpvParam, int flags);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void disable_Click(object sender, EventArgs e)
+        {
+            changeScreenSaver(0);
+        }
+
+        private void Enable_Click(object sender, EventArgs e)
+        {
+            changeScreenSaver(1);
+        }
+
+        private void changeScreenSaver(int state)
+        {
+            
+            SystemParametersInfo(screenSaverActiveFlag, state, ref screenSaverRetVal, screenSaverUpdateProfile);
+        }
+    }
+}
